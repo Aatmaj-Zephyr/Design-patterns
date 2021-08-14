6bd3a2ccab2b560db50e34f2c682ff336112be1a
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-14 13:54:30 +0530

    Create ImprovementFactor.java

---
+++ Observer pattern/Example 1/ImprovementFactor.java
@@ -0,0 +1,28 @@
+public class ImprovementFactor implements Observer{
+    //Tells if the user has increased, decreased or maintained the weight 
+    Double Weight;
+    Double PastWeight;
+    WeighingMachine MyWeighingMachine;
+    ImprovementFactor(WeighingMachine TheWeighingMachine){
+        MyWeighingMachine=TheWeighingMachine;
+        MyWeighingMachine.AddObserver(this);
+        Weight=60.0;
+        PastWeight=60.0;
+    }
+    public void update(){
+       PastWeight=Weight;
+       Weight= MyWeighingMachine.getWeight();
+       check();
+    }
+   private void check(){
+       if((PastWeight>Weight)){
+          System.out.println("You have become thin!");
+       }
+       else if((PastWeight<Weight)){
+           System.out.println("You have grown fat!");
+       }
+       else{
+           System.out.println("You have maintained your weight!");
+       }
+   }
+}
