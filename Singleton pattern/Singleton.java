31eea489b534d7e4781ddfa2525a6a4664fb0555
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-09-12 11:12:02 +0530

    Update Singleton.java

--- Singleton pattern/Singleton.java
+++ Singleton pattern/Singleton.java
@@ -1,5 +1,5 @@
 class singleton{
-  private singleton(){};
+  protected singleton(){}; // only this class and sub-classes have acesss to this.
   public static singleton singletoninstance;
   public static singleton getinstance(){
       if(singletoninstance == null){
